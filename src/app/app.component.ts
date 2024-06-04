7a3d483bc4400f0ab07ed524003661cf723ead56
Heo Sangmin <heo@mapiacompany.com>
2024-06-04 11:40:46 +0900

    refactor: remove all non-related files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { PdfViewer } from 'pdf-viewer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PdfViewer],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [PdfViewer],
+  template: `<pdf-viewer />`
 })
 export class AppComponent {
   title = 'repro-step';
